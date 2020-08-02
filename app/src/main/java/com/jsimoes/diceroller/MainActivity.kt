628d13124c7a27754ee45ff4660c71e9989e4aee
JoaoSimoes <joaombsimoes@gmail.com>
2020-08-02 15:44:34 +0100

    [Feature] Count Up: added new function.

--- app/src/main/java/com/jsimoes/diceroller/MainActivity.kt
+++ app/src/main/java/com/jsimoes/diceroller/MainActivity.kt
@@ -6,22 +6,53 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val incrementValue = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    /**
+     * Bind button actions to events
+     */
     override fun onStart() {
         super.onStart()
 
-        roll.setOnClickListener { v ->
-            rollDice()
-        }
+        roll.setOnClickListener { rollDice() }
+        count.setOnClickListener { countUp() }
     }
 
-    private fun rollDice(){
+    /**
+     * Roll the dice to get a new value
+     */
+    private fun rollDice() {
         Toast.makeText(this, "Rolling dice!", Toast.LENGTH_SHORT).show()
         val randomValue = (1..6).random()
         diceValue.text = randomValue.toString()
+    }
+
+    /**
+     * Count up
+     */
+    private fun countUp() {
+        var newValue = 0
+        val diceCurrentValueStr = diceValue.text.toString()
+        try {
+
+            val currentValueInt = diceCurrentValueStr.toInt()
+
+            if (currentValueInt < 6) {
+                newValue = currentValueInt + incrementValue
+            } else {
+                Toast.makeText(this, "Dice alreay at 6! Can't increment more!", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        } catch (e: NumberFormatException) {
+            newValue = incrementValue
+        }
+
+        diceValue.text = newValue.toString()
     }
 }
