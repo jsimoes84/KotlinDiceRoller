1ce8a07518649566a08b1327a5bd9c875a7979f5
JoaoSimoes <joaombsimoes@gmail.com>
2020-08-03 09:26:48 +0100

    [Feature] Dice Drawable: added function to make the variation of the drawable according to the randomized number.

--- app/src/main/java/com/jsimoes/diceroller/MainActivity.kt
+++ app/src/main/java/com/jsimoes/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jsimoes.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -31,17 +32,22 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Rolling dice!", Toast.LENGTH_SHORT).show()
         val randomValue = (1..6).random()
         currentValue = randomValue
-        updateDiceUI(currentValue)
+        updateDiceUI(diceImage, currentValue)
+        updateDiceUI(diceImage2, getRandomDiceImage())
     }
 
-    private fun updateDiceUI(newValue: Int) {
+    private fun getRandomDiceImage(): Int {
+        return (R.drawable.dice_1..R.drawable.dice_6).random()
+    }
+
+    private fun updateDiceUI(diceObj: ImageView, newValue: Int) {
         when (newValue) {
-            1 -> diceImage.setImageDrawable(getDrawable(R.drawable.dice_1))
-            2 -> diceImage.setImageDrawable(getDrawable(R.drawable.dice_2))
-            3 -> diceImage.setImageDrawable(getDrawable(R.drawable.dice_3))
-            4 -> diceImage.setImageDrawable(getDrawable(R.drawable.dice_4))
-            5 -> diceImage.setImageDrawable(getDrawable(R.drawable.dice_5))
-            6 -> diceImage.setImageDrawable(getDrawable(R.drawable.dice_6))
+            1 -> diceObj.setImageDrawable(getDrawable(R.drawable.dice_1))
+            2 -> diceObj.setImageDrawable(getDrawable(R.drawable.dice_2))
+            3 -> diceObj.setImageDrawable(getDrawable(R.drawable.dice_3))
+            4 -> diceObj.setImageDrawable(getDrawable(R.drawable.dice_4))
+            5 -> diceObj.setImageDrawable(getDrawable(R.drawable.dice_5))
+            6 -> diceObj.setImageDrawable(getDrawable(R.drawable.dice_6))
         }
     }
 
@@ -61,6 +67,6 @@ class MainActivity : AppCompatActivity() {
             currentValue = 1
         }
 
-        updateDiceUI(currentValue)
+        updateDiceUI(diceImage, currentValue)
     }
 }
